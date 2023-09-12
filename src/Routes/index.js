127784f13b3e891ea731b5a95eaa6e30b31b269b
ongdv <ongdv@pusan.ac.kr>
2023-09-12 10:05:20 +0900

    [UPDATE] Menifest 및 배포 설정

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,7 +1,7 @@
 import HomeLayout from 'Components/Layout/HomeLayout/HomeLayout';
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import { Home } from './Pages';
+import { Admin, Device, Home, Provider } from './Pages';
 
 const IndexRouter = () => {
   /* Router */
@@ -13,6 +13,9 @@ const IndexRouter = () => {
     <Routes>
       <Route path="/" element={<HomeLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/provider" element={<Provider />} />
+        <Route path="/device" element={<Device />} />
+        <Route path="/admin" element={<Admin />} />
       </Route>
     </Routes>
   );
